93e209821dbf0b52adc6ce9e7a1cdb918dd14bc2
Astigma90 <git@outpost2147.com>
2025-03-01 12:13:39 +0000

    so far so good

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 import pygame
 from constants import *
-
+from player import *
+from asteroid import *
+from asteroidfield import *
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = updatable, drawable
+    Asteroid.containers = asteroids, updatable, drawable
+    AsteroidField.containers = updatable
+
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000)
-        pygame.display.flip()
+        screen.fill("black")
+        updatable.update(dt)
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                print("Game over!")
+                pygame.quit()
+                return
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+    # print("Starting Asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
